108546a0862c89c0cdff41bc743ddaeee605e215
Andrey Shekhovtsov <nava2007@yandex.ru>
2020-10-24 02:29:00 +0300

    script.js mod relise timer

--- dist/js/script.js
+++ dist/js/script.js
@@ -44,6 +44,58 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Timer
 
-    
+    const deadline = '2020-10-30'; // дата в виде строки
+
+    function getTimeRemaining (endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()), // превращаем строку даты в числовое значение - реальное время
+              days = Math.floor((t / (1000 * 60 * 60 * 24))), // количество дней
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+
+    }
+
+    function getZero (num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        }else{
+            return num;
+        }
+    }
+
+    function setClock (selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock(); // чтобы таймер не возвращался на написанный в html
+
+        function updateClock () {
+            const t = getTimeRemaining(endtime);
+            
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+    }
+
+    setClock('.timer', deadline);
 
 });
